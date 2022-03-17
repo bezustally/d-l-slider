be854957613890ebfc8e2a724a8d0371165135cd
John Bezustally <ample-flood.0p@icloud.com>
2022-03-17 15:18:28 +0400

    feat: added autoplaying slider, next slide onclick

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
 window.addEventListener('DOMContentLoaded', function () {
-  const gallery1 = document.querySelector('#gallery1');
-  const gallery2 = document.querySelector('#gallery2');
-
   class Slider {
     constructor(gallery) {
+      this.isPaused = false;
       this.btnPrev = gallery.querySelector('.gallery__button_prev');
       this.btnNext = gallery.querySelector('.gallery__button_next');
       this.images = gallery.querySelectorAll('.gallery__photo');
       this.i = 0;
       [this.btnPrev, this.btnNext].forEach((button) => button.addEventListener('click', (e) => this.toggleImages(e)));
+      this.slider = gallery.querySelector('.gallery__photos');
+      this.slider.addEventListener('mouseover', (e) => this.toggleAnimation(e));
+      this.slider.addEventListener('mouseout', (e) => this.toggleAnimation(e));
+      this.slider.addEventListener('click', (e) => this.toggleImages(e));
     }
     static showingClass = 'gallery__photo_showed';
     static btnNextClass = 'gallery__button_next';
 
+    toggleAnimation = function (e) {
+      this.isPaused = !this.isPaused;
+    };
+
     toggleImages = function (e) {
-      e.target.disabled = true;
-      const isNext = e.target.classList == Slider.btnNextClass ? true : false;
+      if (this.isPaused) {
+        try {
+          if (e.target.className == 'gallery__photos') this.isPaused = false;
+        } catch {
+          return;
+        }
+      }
+      let isNext = true;
+
+      if (e) {
+        e.target.disabled = true;
+        isNext = e.target.classList == Slider.btnNextClass ? true : false;
+      }
+
       const keyframe = [
         { opacity: 1, transform: 'scale(1.0)' },
         { opacity: 0, transform: 'scale(0.3)' },
       ];
+
       const duration = 700;
 
       this.images[this.i].animate(keyframe, duration);
@@ -35,29 +54,17 @@ window.addEventListener('DOMContentLoaded', function () {
 
       this.images[this.i].classList.add(Slider.showingClass);
       this.images[this.i].animate(keyframe.reverse(), duration);
-      setTimeout(() => (e.target.disabled = false), duration + 50);
+      setTimeout(() => {
+        if (e) e.target.disabled = false;
+      }, duration + 50);
     };
   }
 
+  const gallery1 = document.querySelector('#gallery1');
+  const gallery2 = document.querySelector('#gallery2');
+
   const slider1 = new Slider(gallery1);
   const slider2 = new Slider(gallery2);
+
+  setInterval(() => slider1.toggleImages(), 3000);
 });
-
-/* this.btn.addEventListener('click', () => {
-  this.next()
-}) */
-
-// Сделать из базового слайдера класс Slider.
-
-// После реализации класса мы должны уметь быстро создавать типичные слайдеры:
-
-//  new Slider('.gallery-1');
-//  new Slider('.gallery-2');
-
-// Также должна присутствовать возможность давать слайдеру команды извне, например:
-
-//  let slider2 = new Slider('.gallery-2');
-
-//  setInterval(function(){
-//    slider2.next();
-//  }, 3000);
